aea6ae61393f81c4d5e8b1ee72fe99328f090d7d
Dmytro Kresan <d.kresan@gsi.de>
2017-09-01 09:31:15 +0200

    Rename the LinkDef file - was conflicting with r3bsource directory and leading to wrong rootmap file.

--- sofsource/R3BSofLinkDef.h
+++ sofsource/R3BSofLinkDef.h
@@ -4,21 +4,13 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-//#pragma link C++ class R3BUcesbSource+;
-//#pragma link C++ class R3BReader+;
-//#pragma link C++ class R3BUnpackReader+;
-//#pragma link C++ class R3BNeulandTamexReader+;
-//#pragma link C++ class R3BNeulandTacquilaReader+;
 #pragma link C++ class R3BSofATMadcReader+;
 #pragma link C++ class R3BSofComRefReader+;
-//#pragma link C++ class R3BSofATReader+;
 #pragma link C++ class R3BSofSciReader+;
 
-//#pragma link C++ class EXT_STR_h101_unpack_t;
-//#pragma link C++ class EXT_STR_h101_raw_nnp_onion_t;
-//#pragma link C++ class EXT_STR_h101_raw_nnp_tamex_onion_t;
 #pragma link C++ class EXT_STR_h101_SOFTOFW_onion_t;
 #pragma link C++ class EXT_STR_h101_SOFSCI_onion_t;
 #pragma link C++ class EXT_STR_h101_SOFCOMREF_onion_t;
 #pragma link C++ class EXT_STR_h101_SOFAT_MADC_onion_t;
+
 #endif
