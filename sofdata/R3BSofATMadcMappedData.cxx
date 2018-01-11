b619f7b42694c6cea465aaaac95da941a213c765
Audrey Chatillon <audrey.chatillon@gmail.com>
2018-01-11 11:32:10 +0100

    definitition of a new data format for the R3BSofATMadeMappedData

--- sofdata/R3BSofATMadcMappedData.cxx
+++ sofdata/R3BSofATMadcMappedData.cxx
@@ -1,16 +1,18 @@
 #include "R3BSofATMadcMappedData.h"
 
 // for the data analysis of the SOFIA - ACTIVE TARGET
-// Introduced by Audrey in March 2017
 
 
 R3BSofATMadcMappedData::R3BSofATMadcMappedData()
-  : fEnergy(std::vector<UInt_t>(0))
+  : fAnode(0)
+  , fEnergy(0)
 {
 }
 
-R3BSofATMadcMappedData::R3BSofATMadcMappedData(	UInt_t nCh)
-  : fEnergy(std::vector<UInt_t>(nCh))
+R3BSofATMadcMappedData::R3BSofATMadcMappedData(UChar_t a,
+					       UInt_t  e)
+  : fAnode(a)
+  , fEnergy(e)
 {
 }
 
