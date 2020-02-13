2f37145385eaaad0daf48f9044d2d3871f853688
Audrey Chatillon <audrey.chatillon@gmail.com>
2020-02-13 16:21:20 +0100

    add R3BSofSciRawTofPar class

---
+++ sci/R3BSofSciRawTofPar.cxx
@@ -0,0 +1,106 @@
+#include "R3BSofSciRawTofPar.h"
+
+#include "FairLogger.h"
+#include "FairParamList.h"
+
+#include "TArrayF.h"
+#include "TMath.h"
+#include "TString.h"
+
+#include <iostream>
+
+#define MAX_RAWTOFPAR NUMBER_OF_SOFSCI_DETECTORS * NUMBER_OF_SOFSCI_TOF
+
+using std::cout;
+using std::endl;
+
+// ---- Standard Constructor ---------------------------------------------------
+R3BSofSciRawTofPar::R3BSofSciRawTofPar(const char* name, const char* title, const char* context)
+  : FairParGenericSet(name, title, context)
+  , fNumSignals(0)
+  , fNumParsPerSignal(0)
+{
+  fAllSignalsRawTofParams = new TArrayF(MAX_RAWTOFPAR);
+}
+
+// ----  Destructor ------------------------------------------------------------
+R3BSofSciRawTofPar::~R3BSofSciRawTofPar()
+{
+    clear();
+    if (fAllSignalsRawTofParams)
+    {
+        delete fAllSignalsRawTofParams;
+        fAllSignalsRawTofParams == NULL;
+    }
+}
+
+// ----  Method clear ----------------------------------------------------------
+void R3BSofSciRawTofPar::clear()
+{
+    status = kFALSE;
+    resetInputVersions();
+}
+
+// ----  Method putParams ------------------------------------------------------
+void R3BSofSciRawTofPar::putParams(FairParamList* list)
+{
+    LOG(INFO) << "R3BSofSciRawTofPar::putParams() called";
+    if (!list)
+    {
+        return;
+    }
+
+    Int_t array_size = fNumSignals * fNumParsPerSignal;
+    LOG(INFO) << "Array Size: " << array_size;
+
+    fAllSignalsRawTofParams->Set(array_size);
+
+    list->add("RawTofPar", *fAllSignalsRawTofParams);
+    list->add("nSignalsRawTofPar", fNumSignals);
+    list->add("nRawTofParsPerSignal", fNumParsPerSignal);
+}
+
+// ----  Method getParams ------------------------------------------------------
+Bool_t R3BSofSciRawTofPar::getParams(FairParamList* list)
+{
+    LOG(INFO) << "R3BSofSciRawTofPar::getParams() called";
+    if (!list)
+    {
+        return kFALSE;
+    }
+    if (!list->fill("nSignalsRawTofPar", &fNumSignals))
+    {
+        return kFALSE;
+    }
+    if (!list->fill("nRawTofParsPerSignal", &fNumParsPerSignal))
+    {
+        return kFALSE;
+    }
+
+    Int_t array_size = fNumSignals * fNumParsPerSignal;
+    LOG(INFO) << "Array Size: " << array_size;
+    fAllSignalsRawTofParams->Set(array_size);
+
+    if (!(list->fill("RawTofPar", fAllSignalsRawTofParams)))
+    {
+        LOG(INFO) << "---Could not initialize fAllSignalsRawTofParams";
+        return kFALSE;
+    }
+
+    return kTRUE;
+}
+
+// ----  Method printParams ----------------------------------------------------
+void R3BSofSciRawTofPar::printParams()
+{
+    LOG(INFO) << "R3BSofSciRawTofPar: SofSciRawTof Parameters: ";
+    Int_t array_size = fNumSignals * fNumParsPerSignal;
+
+    cout << "--- --------------------------------------------" << endl;
+    cout << "--- Single Tcal RawTof Parameters :  " << endl;
+    cout << "--- --------------------------------------------" << endl;
+    for (Int_t param=0; param < array_size; param++)
+      {
+	cout << "LIMIT " << param << " = " << fAllSignalsRawTofParams->GetAt(param) << endl;
+      }
+}
--- sofdata/sciData/R3BSofSciSingleTcalData.h
+++ sofdata/sciData/R3BSofSciSingleTcalData.h
@@ -19,7 +19,7 @@ class R3BSofSciSingleTcalData : public TObject
     // Getters
     inline const Double_t& GetRawTimeNs(UShort_t det) const { return fRawTimeNs[det-1]; }
     inline const Double_t& GetRawPosNs(UShort_t det)  const { return fRawPosNs[det-1]; }
-#ifdef NUMBER_OF_TOF
+#ifdef NUMBER_OF_SOFSCI_TOF
     inline const Double_t& GetRawTofNs(UShort_t rank) const { return fRawTofNs[rank];}
 #endif
 
@@ -27,7 +27,7 @@ class R3BSofSciSingleTcalData : public TObject
     void SetRawTimeNs(UShort_t det, Double_t time)  {fRawTimeNs[det-1]=time;}
     void SetRawPosNs(UShort_t det, Double_t pos)    {fRawPosNs[det-1]=pos;}
     void SetMultPerDet(UShort_t det, UShort_t m)    {fMultPerDet[det-1]=m;}
-#ifdef NUMBER_OF_TOF
+#ifdef NUMBER_OF_SOFSCI_TOF
     void SetRawTofNs(UShort_t rank, Double_t tof)   {fRawTofNs[rank]=tof;} 
     void SetMultPerToF(UShort_t rank, UShort_t m)    {fMultPerDet[rank]=m;} 
 #endif
@@ -36,9 +36,9 @@ class R3BSofSciSingleTcalData : public TObject
     Double_t fRawTimeNs[NUMBER_OF_SOFSCI_DETECTORS];           // 0.5*(Tleft + Tright)
     Double_t fRawPosNs[NUMBER_OF_SOFSCI_DETECTORS];            // Tleft - Tright
     UShort_t fMultPerDet[NUMBER_OF_SOFSCI_DETECTORS];        // number of hits with the proper pos 
-#ifdef NUMBER_OF_TOF
-    Double_t fRawTofNs[NUMBER_OF_TOF];
-    UShort_t fMultPerTof[NUMBER_OF_TOF];              // number of hits with the proper tof 
+#ifdef NUMBER_OF_SOFSCI_TOF
+    Double_t fRawTofNs[NUMBER_OF_SOFSCI_TOF];
+    UShort_t fMultPerTof[NUMBER_OF_SOFSCI_TOF];              // number of hits with the proper tof 
 #endif
   public:
     ClassDef(R3BSofSciSingleTcalData, 2)
