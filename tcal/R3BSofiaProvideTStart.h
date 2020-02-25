9c5d9e1f39a1938855689f2b37415b4a298d6923
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2020-02-25 13:14:22 +0100

    Provide TStart to R3BEventHeader

---
+++ tcal/R3BSofiaProvideTStart.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+#ifndef R3BROOT_R3BSOFIAPROVIDETSTART_H
+#define R3BROOT_R3BSOFIAPROVIDETSTART_H
+
+#include "FairTask.h"
+#include "R3BSofSciTcalData.h"
+#include "TCAConnector.h"
+
+class R3BEventHeader;
+
+class R3BSofiaProvideTStart : public FairTask
+{
+  public:
+    R3BSofiaProvideTStart();
+
+    InitStatus Init() override;
+    void Exec(Option_t*) override;
+
+  private:
+    TCAInputConnector<R3BSofSciTcalData> fSofSciCalData;
+    R3BEventHeader* fEventHeader;
+
+    bool IsBeam() const;
+    Double_t GetTStart() const;
+
+    ClassDefOverride(R3BSofiaProvideTStart, 0)
+};
+
+#endif // R3BROOT_R3BSOFIAPROVIDETSTART_H
--- tcal/SofTcalLinkDef.h
+++ tcal/SofTcalLinkDef.h
@@ -1,15 +1,17 @@
+// clang-format off
 #ifdef __CINT__
 
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class R3BSofTcalContFact + ;
-#pragma link C++ class R3BSofTcalPar + ;
-#pragma link C++ class R3BSofSciMapped2TcalPar + ;
-#pragma link C++ class R3BSofSciMapped2Tcal + ;
-#pragma link C++ class R3BSofToFWMapped2TcalPar + ;
-#pragma link C++ class R3BSofToFWMapped2Tcal + ;
+#pragma link C++ class R3BSofTcalContFact+;
+#pragma link C++ class R3BSofTcalPar+;
+#pragma link C++ class R3BSofSciMapped2TcalPar+;
+#pragma link C++ class R3BSofSciMapped2Tcal+;
+#pragma link C++ class R3BSofToFWMapped2TcalPar+;
+#pragma link C++ class R3BSofToFWMapped2Tcal+;
+#pragma link C++ class R3BSofiaProvideTStart+;
 
 
 #endif
