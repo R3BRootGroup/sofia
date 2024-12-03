8ba398bf9df589117c85e5f7ff459f05656c4205
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2024-12-03 23:50:37 +0100

    Cleaning repository
    Update github workflows
    
    Update github workflows
    
    Update to v4
    
    rm .githubfiles/dart_experimental_nov22p1.cfg
    
    Update to start the CI-CD every night
    
    Update to start the CI-CD every night
    
    Update to jan24p1
    
    Removed .githubfiles
    
    Added .githubfiles
    
    Changed .githubfiles

--- apply-format.sh
+++ apply-format.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-CLANG_FORMAT_BIN="clang-format-15"
-
-FILES=$(find . -type f \( -iname "*.h" ! -iname "*.pb.h" ! -iname "*LinkDef.h" ! -iname "ext_h101_*.h" -o -iname "*.hpp" -o -iname "*.cxx" -o -iname "*.cpp" -o -iname "*.tpl" -o -iname "*.c" -o -iname "*.C" \) -not \( -path "./macros/*" -prune \))
-
-$CLANG_FORMAT_BIN -i $FILES
-echo "Clang-format has been applied over all files. Use git add -A ; git commit -m \"clang-format all files\""
+find  -type f | grep -e '.(\.C\|\.cpp\|\.cxx\|\.h)$'  | grep -v ^./macros | xargs -L 1 clang-format-15 -i 
+echo "Use git add -A ; git commit -m \"clang-format all files\" --author=\"white space <whitespace@example.com>\" to commit changes."
