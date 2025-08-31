8bd28a1f67e9f533abd121d07167e905debe4392
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2025-08-31 09:59:49 +0200

    Update Readme and license
    Update
    
    Update
    
    Update workflow
    
    Update workflow
    
    Update workflow
    
    Update workflow
    
    Update workflow
    
    Update workflow
    
    Update workflow
    
    Minor change

--- sci/SofSciLinkDef.h
+++ sci/SofSciLinkDef.h
@@ -1,4 +1,19 @@
 // clang-format off
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 University of Santiago de Compostela                  *
+ *   Copyright (C) 2019 CEA-DAM-DIF, Université Paris-Saclay                  *
+ *   Copyright (C) 2023 University of Coruña                                  *
+ *   Copyright (C) 2019-2025 Members of R3B Collaboration                     *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
 
 #ifdef __CINT__
 
