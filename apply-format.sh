60f9846da968b8c59f970644ed11423af360a573
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2025-09-06 12:58:42 +0200

    feat(clang-format):Remove old folders

--- apply-format.sh
+++ apply-format.sh
@@ -16,7 +16,7 @@
 
 #!/usr/bin/env bash
 find . -type f \( -name "*.cpp" -o -name "*.cxx" -o -name "*.C" -o -name "*.h" \) \
-    ! -path "./macros" ! -path "./sofia" ! -path "./frs" ! -path "./asyeos" ! -path "./glad-tpc" -print0 |
+    ! -path "./macros/*" -print0 |
     xargs -0 -L 1 clang-format-16 -i
 
 echo "Use git add -A ; git commit -m \"clang-format all files\" --author=\"white space <whitespace@example.com>\" to commit changes."
